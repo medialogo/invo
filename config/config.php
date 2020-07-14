c7ba7b028dd2ded67c83c05da82ffb7676878ddb
t.ikemi (4230) <ikemi@medialogo.com>
2020-07-15 04:04:53 +0900

    v4.0 got and tried

--- config/config.php
+++ config/config.php
@@ -3,17 +3,30 @@ declare(strict_types=1);
 
 use Phalcon\Config;
 
+//You may load configs from config.ini file.
+//return new Phalcon\Config\Adapter\Ini("../config.ini");
+$dotenv = Dotenv\Dotenv::createImmutable('C:\dev15\phalcon\tutorial\invo');
+$dotenv->load();
 return new Config([
     'database' => [
-        'adapter' => getenv('DB_ADAPTER'),
-        'host' => getenv('DB_HOST'),
-        'username' => getenv('DB_USERNAME'),
-        'password' => getenv('DB_PASSWORD'),
-        'dbname' => getenv('DB_DBNAME'),
-        'charset' => getenv('DB_CHARSET'),
+        //'adapter' => $_ENV['DB_ADAPTER'],
+        'adapter' => 'Mysql',
+        'host' => $_ENV['DB_HOST'],
+        'username' => $_ENV['DB_USERNAME'],
+        'password' => $_ENV['DB_PASSWORD'],
+        'dbname' => $_ENV['DB_DBNAME'],
+        'charset' => 'utf8',//$_ENV['DB_CHARSET'],
     ],
     'application' => [
-        'viewsDir' => getenv('VIEWS_DIR'),
-        'baseUri' => getenv('BASE_URI'),
+        'controllersDir' => $_ENV['CONTROLLERS_DIR'],
+        'modelsDir' => $_ENV['MODELS_DIR'],
+        'pluginsDir' => $_ENV['PLUGINS_DIR'],
+        'formsDir' => $_ENV['FORMS_DIR'],
+        'providersDir' => $_ENV['PROVIDERS_DIR'],
+        'migrationsDir' => 'C:\dev15\phalcon\tutorial\invo\db\migrations',
+        'viewsDir' => $_ENV['VIEWS_DIR'],
+        'baseUri' => $_ENV['BASE_URI'],
     ],
 ]);
+
+
